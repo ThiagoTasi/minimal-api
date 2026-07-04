9579d30caf4ed9cf74a634f708fd70621d0a29fe
Thiago Tasi <thiagosilvateo@gmail.com>
2026-07-03 22:38:29 -0300

    Configuração inicial do banco de dados funcionando

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,35 @@
+
+
+   using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
-    
+    private readonly IConfiguration _configuracaoAppSettings;
+
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+        _configuracaoAppSettings = configuracaoAppSettings;
+    }
+
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Coloquei as chaves abrindo e fechando o bloco do if dele
+        if (!optionsBuilder.IsConfigured)
+        {
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+            
+            if (!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(stringConexao, ServerVersion.AutoDetect(stringConexao));
+            }
+        }
+    }   
 }
+    
+    
+
--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// A CONFIGURAÇÃO DO BANCO TEM QUE FICAR AQUI (ANTES DO BUILD!)
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    var stringConexao = builder.Configuration.GetConnectionString("mysql");
+    options.UseMySql(stringConexao, ServerVersion.AutoDetect(stringConexao));
+});
+
+// O BUILD VEM DEPOIS QUE TUDO FOI CONFIGURADO
 var app = builder.Build();
 
 app.MapGet("/", () => "Olá pessoal!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", (LoginDTO loginDTO) =>
 {
-    if (loginDTO.Email== "admin@teste.com" && loginDTO.Senha == "123456")
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
     {
         return Results.Ok("Login com sucesso!");
     }
